8537a2543366cdfad69c64d34da4d7d67d9bf271
clsid2 <4704996+clsid2@users.noreply.github.com>
2022-08-10 13:10:22 +0200

    Add support for WebP images.
    Also loop images so they have a duration.

--- demuxer/Demuxers/LAVFDemuxer.cpp
+++ demuxer/Demuxers/LAVFDemuxer.cpp
@@ -207,6 +207,7 @@ static LPCWSTR wszImageExtensions[] = {
     L".tga",                     // TGA
     L".bmp",                     // BMP
     L".j2c",                     // JPEG2000
+    L".webp",                    // WebP
 };
 
 static LPCWSTR wszBlockedExtensions[] = {L".ifo", L".bup"};
@@ -308,6 +309,8 @@ trynoformat:
 
     LPWSTR extension = pszFileName ? PathFindExtensionW(pszFileName) : nullptr;
 
+    bool imageformat = false;
+
     const AVInputFormat *inputFormat = nullptr;
     if (format)
     {
@@ -320,6 +323,7 @@ trynoformat:
         {
             if (_wcsicmp(extension, wszImageExtensions[i]) == 0)
             {
+                imageformat = true;
                 if (byteContext)
                 {
                     inputFormat = av_find_input_format("image2pipe");
@@ -355,6 +359,10 @@ trynoformat:
     av_dict_set(&options, "reconnect", "1", 0);         // for http, reconnect if we get disconnected
     av_dict_set(&options, "referer", fileName, 0);      // for http, send self as referer
     av_dict_set(&options, "skip_clear", "1", 0);        // mpegts program handling
+    if (imageformat) {
+        av_dict_set(&options, "loop", "1", 0);          // loop images
+        av_dict_set(&options, "framerate", "1", 0);     // image framerate
+    }
 
     // send global side data to the decoder
     av_format_inject_global_side_data(m_avFormat);
